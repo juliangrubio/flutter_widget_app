35b13adc5cb646d1489c890506f9cc84af9a23df
julian <juliangrubio@gmail.com>
2023-11-12 15:05:51 -0300

    card finished

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String, dynamic>>[
+  {'elevation': 0.5, 'label': 'Elevation 0.5'},
+  {'elevation': 1.0, 'label': 'Elevation 1.0'},
+  {'elevation': 2.0, 'label': 'Elevation 2.0'},
+  {'elevation': 3.0, 'label': 'Elevation 3.0'},
+  {'elevation': 4.0, 'label': 'Elevation 4.0'},
+  {'elevation': 5.0, 'label': 'Elevation 5.0'},
+  {'elevation': 6.0, 'label': 'Elevation 6.0'},
+];
+
 class CardsScreen extends StatelessWidget {
   static const String name = 'cards_screen';
   const CardsScreen({super.key});
@@ -10,7 +20,188 @@ class CardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cards Screen'),
       ),
-      body: const Placeholder(),
+      body: const _CardsView(),
     );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView();
+
+  @override
+  Widget build(BuildContext context) {
+    // SINO APARECE UNA GRAFICA AMARILLA Y NEGRA EN EL EMULADOR ES PORQUE SE DESBORDO
+    // EL SCREEN DE TANTOS ELEMENTOS Y NO CABEN, PARA ESO USO SINGLECHILDSCROOLVIEW
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map((card) => _CardType1(
+                elevation: card['elevation'],
+                label: card['label'],
+              )),
+          // PARA SIMULAR EL DESBORDE
+          // ...cards.map((card) => _CardType1(
+          //       elevation: card['elevation'],
+          //       label: card['label'],
+          //     ))
+          ...cards.map((card) => _CardType2(
+                elevation: card['elevation'],
+                label: card['label'],
+              )),
+
+          ...cards.map((card) => _CardType3(
+                elevation: card['elevation'],
+                label: card['label'],
+              )),
+
+          ...cards.map((card) => _CardType4(
+                elevation: card['elevation'],
+                label: card['label'],
+              )),
+          // LA IDEA DE ESTO ES QUE EL ULTIMO WIDGET QUE SE MUESTRE SEA ESTE
+          // PARA QUE QUEDE UN ESPACIO AL FONDO DE LA LISTA
+          const SizedBox(
+            height: 30,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: elevation,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                    icon: const Icon(Icons.more_vert_outlined),
+                    onPressed: () {},
+                  )),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(label),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(12)),
+            // borderRadius: BorderRadius.circular(12),
+            side: BorderSide(color: colors.outline)),
+        elevation: elevation,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                    icon: const Icon(Icons.more_vert_outlined),
+                    onPressed: () {},
+                  )),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text('$label - Outline'),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+        color: colors.surfaceVariant,
+        elevation: elevation,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                    icon: const Icon(Icons.more_vert_outlined),
+                    onPressed: () {},
+                  )),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text('$label - Filled'),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+        // ESTE CLIP EVITA QUE LOS HIJOS SE SALGAN DE SU CONTENIDO
+        clipBehavior: Clip.hardEdge,
+        elevation: elevation,
+        child: Stack(
+          children: [
+            Image.network(
+              'https://picsum.photos/id/${elevation.toInt()}/600/350',
+              height: 350,
+              fit: BoxFit.cover,
+            ),
+            Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20))
+                    ),
+                  child: IconButton(
+                    icon: const Icon(Icons.more_vert_outlined),
+                    onPressed: () {},
+                  ),
+                )),
+            // Align(
+            //   alignment: Alignment.bottomLeft,
+            //   child: Text('$label - Image'),
+            // )
+          ],
+        ));
   }
 }
