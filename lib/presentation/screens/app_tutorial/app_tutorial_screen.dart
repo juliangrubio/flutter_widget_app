f88b526caefb2eea639411fd0bf691c93227dada
Julian Rubio <juliangrubio@gmail.com>
2023-12-04 17:47:35 -0300

    pageview finished

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,11 +1,139 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class AppTutorial extends StatelessWidget {
-  static const String name = 'tutorial_screen'; 
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+      'Busca la comida',
+      'Mollit ut officia nostrud id nulla cupidatat commodo consectetur sit et.',
+      'assets/Images/1.png'),
+  SlideInfo(
+      'Entrega rapida',
+      'Consequat id proident do labore pariatur et sunt esse id.',
+      'assets/Images/2.png'),
+  SlideInfo(
+      'Disfruta la comida',
+      'Deserunt enim et adipisicing adipisicing fugiat sit pariatur.',
+      'assets/Images/3.png'),
+];
+
+class AppTutorial extends StatefulWidget {
+  static const String name = 'tutorial_screen';
   const AppTutorial({super.key});
 
   @override
+  State<AppTutorial> createState() => _AppTutorialState();
+}
+
+class _AppTutorialState extends State<AppTutorial> {
+  // late final PageController pageviewController;
+  // O PUEDE SER ASI PARA EVITARNOS EL LATE
+  final PageController pageviewController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageviewController.addListener(() {
+      final page = pageviewController.page ?? 0;
+      if (!endReached && page >= (slides.length - 1.5)) {
+        setState(() {
+          endReached = true;
+        });
+      }
+      // print('${pageviewController}');
+    });
+  }
+
+  @override
+  void dispose() {
+    pageviewController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // SI NO NECESITO EL BOTON SKIP PUEDO REMOVER EL POSITIONED Y EL STACK
+      // Y PASARLE DIRECTAMENTE EL PAGE VIEW EN EL BODY
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageviewController,
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map((slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl))
+                .toList(),
+          ),
+          Positioned(
+            top: 40,
+            right: 30,
+            child: TextButton(
+              child: const Text('Skip'),
+              onPressed: () => context.pop(),
+            ),
+          ),
+          endReached
+              ? Positioned(
+                  bottom: 30,
+                  right: 30,
+                  child: FadeInRight(
+                    from: 15,
+                    delay: const Duration(seconds: 1),
+                    child: FilledButton(
+                      child: const Text('Comenzar'),
+                      onPressed: () {},
+                    ),
+                  ))
+              : const SizedBox()
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+  const _Slide(
+      {required this.title, required this.caption, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image(image: AssetImage(imageUrl)),
+          const SizedBox(height: 20),
+          Text(title, style: titleStyle),
+          const SizedBox(height: 20),
+          Text(
+            caption,
+            style: captionStyle,
+          )
+        ],
+      )),
+    );
   }
 }
